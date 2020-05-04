c016309ffb91db8374b49e79e0511427a46d583f
Sandip75 <sk1957339@gmail.com>
2020-05-04 23:17:37 +0530

    blog

--- db/schema/admin.js
+++ db/schema/admin.js
@@ -1,25 +1,25 @@
-const mongoose = require("../connect");
-const Schema = mongoose.Schema;
-const { schemaNames } = require("./../../config");
-const adminSchemaConfig = {
-    fullname: {
-        type: String
-        // enum: ['aniket', 'sandip'],
-        // required: true,
-        // unique: true,
+// const mongoose = require("../connect");
+// const Schema = mongoose.Schema;
+// const { schemaNames } = require("./../../config");
+// const adminSchemaConfig = {
+//     fullname: {
+//         type: String
+//         // enum: ['aniket', 'sandip'],
+//         // required: true,
+//         // unique: true,
 
-     }
-    // updateAt: {
-    //     type: Date,
-    //     default: Date.now
-    // }
-};
-const adminSchemaOptions = {
-    timestamp: {
-        createdAt: 'ct',
-        updatedAt: 'ut',
-    }
-};
-const AdminSchema = new Schema(adminSchemaConfig, adminSchemaOptions);
-const AdminModel = mongoose.model(schemaNames.ADMIN_SCHEMA, AdminSchema);
-module.exports = AdminModel;
+//      }
+//     // updateAt: {
+//     //     type: Date,
+//     //     default: Date.now
+//     // }
+// };
+// const adminSchemaOptions = {
+//     timestamp: {
+//         createdAt: 'ct',
+//         updatedAt: 'ut',
+//     }
+// };
+// const AdminSchema = new Schema(adminSchemaConfig, adminSchemaOptions);
+// const AdminModel = mongoose.model(schemaNames.ADMIN_SCHEMA, AdminSchema);
+// module.exports = AdminModel;

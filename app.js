48a553adaa18ff06e2515b5b164a50c4d5d134cc
Sandip75 <sk1957339@gmail.com>
2020-05-04 23:20:04 +0530

    blog

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set("view engine", "ejs");
 app.use("/", require("./routers/index"));
 
 //#endregion
-
-app.listen(4000, () => {
+const port = process.env.PORT || 4000
+app.listen(port, () => {
   console.log("sever started");
 });
